afbc734e7a3d202f40e8036dee102a5532d8d3fc
Taavi Sangel <taavi.sangel@gmail.com>
2018-04-05 13:26:25 +0300

    finished relations between article and author

--- server/src/schemas/article/article-resolver.ts
+++ server/src/schemas/article/article-resolver.ts
@@ -1,6 +1,11 @@
-import articleDb from '../../db/article';
+import articleDb, { Article } from '../../db/article';
+import authorDb from '../../db/author';
 
 export default {
+	Article: {
+		author: ({ authorId }: Article) => authorDb.findById(authorId),
+	},
+
 	Query: {
 		article: (_: any, { id }: any) => articleDb.findById(id),
 		articleList: articleDb.findAll,
--- server/src/schemas/author/author-resolver.ts
+++ server/src/schemas/author/author-resolver.ts
@@ -1,6 +1,11 @@
-import authorDb from '../../db/author';
+import authorDb, { Author } from '../../db/author';
+import articleDb from '../../db/article';
 
 export default {
+	Author: {
+		articles: ({ id }: Author) => articleDb.findWhere('authorId', id),
+	},
+
 	Query: {
 		author: (_: any, { id }: any) => authorDb.findById(id),
 	},
