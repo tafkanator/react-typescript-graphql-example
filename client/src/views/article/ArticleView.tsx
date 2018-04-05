894c7f6b18abe0da3bd0f20bc60010e865d4caf0
Taavi Sangel <taavi.sangel@gmail.com>
2018-04-05 22:39:01 +0300

    improved visuals

--- client/src/views/article/ArticleView.tsx
+++ client/src/views/article/ArticleView.tsx
@@ -4,15 +4,16 @@ import './article-view.css';
 
 export default () => (
 	<article className="article-view">
-		<h2 className="article-view__title">Tere 123</h2>
-
 		<div className="view">
+			<h2 className="article-view__title">Tere 123</h2>
 			<img className="article-view__thumb" src="https://picsum.photos/800/500?image=1018" alt="Tere 123" />
 
 			<p className="article-view__content">Lorem ipsum</p>
 			<p className="article-view__prefix">Igor Viigor</p>
 
-			<Link to="/">Back to list view</Link>
+			<Link to="/" className="article-view__footer-link">
+				Back to list view
+			</Link>
 		</div>
 	</article>
 );
